0f138c27b65a159aae0deaa028d30498db3d61af
augustocognition <augusto.uriona@cognitionhq.com>
2025-02-12 11:36:47 -0300

    fix validations

--- src/main/java/com/nevent/challenge/dto/OrderRequest.java
+++ src/main/java/com/nevent/challenge/dto/OrderRequest.java
@@ -1,6 +1,5 @@
 package com.nevent.challenge.dto;
 
-import com.nevent.challenge.entity.Status;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
